6643fb587b7dee4f1ed18ce5c49fcf98db8890e6
Debajyoti Mondal <debajyoti55561@gmail.com>
2026-03-11 13:22:23 +0530

    Refactor ModelHandler to improve component management
    Removed automatic directory creation for model path and added max_comp attribute for component limit.

--- model.py
+++ model.py
@@ -11,13 +11,13 @@ import numpy as np
 class ModelHandler:
     def __init__(self, model_path):
         self.model_path = Path(model_path)
-        self.model_path.mkdir(parents=True, exist_ok=True)
         # pca_path = self.model_path/"pca_model.pkl"
         self.pca_path = self.model_path / "pca_model.joblib"
         self.kmeans_path = self.model_path / "kmeans_model.joblib"
         self.data = None
         self.kmeans_obj = None
         self.pca_obj = None
+        self.max_comp = 10
 
     def save_model(self):
         if self.pca_obj and self.kmeans_obj:
@@ -31,30 +31,25 @@ class ModelHandler:
         if not self.data or len(self.data) == 0:
             return []
 
-        # Use a lightweight transformer model
-        transformer = SentenceTransformer('all-MiniLM-L6-v2')
-        final_conent = []
-
-        for item in self.data:
-            emb = transformer.encode(item, normalize_embeddings=True)
-            final_conent.append(emb)
-        vector_content = np.array(final_conent)
+        emb = self.data
 
         # PCA Reduction (ensure components <= samples)
-        n_comp = min(len(self.data), 10)
+        n_comp = min(len(self.data), self.max_comp)
 
         if not self.pca_obj:
             self.pca_obj = PCA(n_components=n_comp)
             print("pca created")
-            x_reduced = self.pca_obj.fit_transform(vector_content)
+            x_reduced = self.pca_obj.fit_transform(emb)
         else:
-            x_reduced = self.pca_obj.transform(vector_content)
+            x_reduced = self.pca_obj.transform(emb)
 
         # KMeans Clustering (ensure clusters <= samples)
         n_clust = min(len(self.data), 5)
         if  not self.kmeans_obj:
             self.kmeans_obj = KMeans(n_clusters=n_clust, n_init='auto')
             print("kmeans created")
-        clusters = self.kmeans_obj.fit_predict(x_reduced)
+            clusters = self.kmeans_obj.fit_predict(x_reduced)
+        else:
+            clusters = self.kmeans_obj.predict(x_reduced)
 
         return clusters
